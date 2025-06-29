609415ef675ab22968c4fd76ca1245ff2cf14f11
Shreyash <shreyash.bhatkar@oracle.com>
2025-06-30 02:07:23 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * University of Illinois
+ * Object-Oriented Data Structures in C++ - Week 2
+ * Hello World Project
+ *
+ * @author Eric Huber
+**/
+
+#include <iostream> // Include for std::cout
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Greetings from Illinois!" << std::endl;
+    return 0;
+}
